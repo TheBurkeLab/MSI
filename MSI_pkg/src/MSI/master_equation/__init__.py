71a16dcb11ac0f2e9e55fd70e0288069e615e6e3
pjsingal <59570055+pjsingal@users.noreply.github.com>
2024-03-06 17:33:23 -0500

    Add files via upload

---
+++ MSI_pkg/src/MSI/master_equation/__init__.py
@@ -0,0 +1,6 @@
+################### NOT IMPORTANT ####################
+
+
+__all__ = ['master_equation_six_parameter_fit', 'master_equation']
+from . import master_equation_six_parameter_fit 
+from . import master_equation
